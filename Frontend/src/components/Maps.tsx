6e0471a8a86aedd9d83e399e3c9b18bdd3085f32
SilentWolf6662 <52424766+SilentWolf6662@users.noreply.github.com>
2024-10-30 14:18:19 +0100

    Changed pin to image and made clustering to work

--- Frontend/src/components/Maps.tsx
+++ Frontend/src/components/Maps.tsx
@@ -1,29 +1,49 @@
 import "ol/ol.css";
 import "./Maps.scss"; // Import the SCSS file
 
+import {
+  Circle as CircleStyle,
+  Fill,
+  Icon,
+  Stroke,
+  Style,
+  Text,
+} from 'ol/style.js';
+import {Cluster, OSM, Vector as VectorSource} from 'ol/source.js';
+import {Extent, createEmpty, extend, getHeight, getWidth} from 'ol/extent.js';
+import { IonButton, IonIcon, IonImg, IonModal } from "@ionic/react";
+import { MousePosition, defaults as defaultControls } from "ol/control.js";
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
 import { fromLonLat, toLonLat } from "ol/proj";
+import { useEffect, useState } from "react";
+
 import Feature from "ol/Feature";
-import MousePosition from "ol/control/MousePosition.js";
-import OSM from "ol/source/OSM";
 import { default as OlMap } from "ol/Map";
 import Point from "ol/geom/Point";
-import TileLayer from "ol/layer/Tile";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import View from "ol/View";
-import { createStringXY } from "ol/coordinate.js";
-import { defaults as defaultControls } from "ol/control.js";
-import { useEffect, useState } from "react";
-import { IonButton, IonIcon, IonModal } from "@ionic/react";
-import { close } from "ionicons/icons";
-import useRequestData from "../hooks/useRequestData";
 import ShowPinModal from "../modals/ShowPinModal";
+import View from "ol/View";
+import { close } from "ionicons/icons";
+import { createStringXY } from "ol/coordinate.js";
+import useRequestData from "../hooks/useRequestData";
 
 interface MapProps {
   APIurl: string;
 }
 
-const debug = false; // Set this to false to disable logging
+const debug = true; // Set this to false to disable logging
+
+const iconSrc: string = "http://localhost:8100/icons/webp/ping1.webp"; // Replace with the correct URL to the pin icon
+
+/* region Predefined styles for clustering */
+
+const distanceBetweenPinsBeforeClustering = 20; // Distance between pins before clustering
+const clusterRadius = 5; // base radius of the cluster circle it is multiplied by the number of pins in the cluster
+const clusterMultiplier = 0.8; // Multiplier for the cluster circle radius after the calculation
+const clusterFillColor = '#236477'; // Color of the cluster circle
+const clusterStrokeColor = '#fff'; // Color of the cluster circle stroke
+const clusterTextColor = '#fff'; // Color of the text/number inside the cluster circle
+
+/* endregion */
 
 const mousePositionControl = new MousePosition({
   coordinateFormat: createStringXY(4),
@@ -33,6 +53,44 @@ const mousePositionControl = new MousePosition({
   className: "custom-mouse-position",
   target: document.getElementById("mouse-position") as HTMLElement,
 });
+
+function createStyle(size: number = 0.3) {
+    return new Style({
+      image: new Icon({
+        anchor: [0.5, 0.96],
+        crossOrigin: 'anonymous',
+        src: iconSrc,
+        scale: size
+      })
+    })
+  }
+
+function createClusterStyle(feature: Feature) {
+  const size = feature.get('features').length;
+  let style;
+  if (size > 1) {
+    style = new Style({
+      image: new CircleStyle({
+        radius: (clusterRadius * size) * clusterMultiplier,
+        stroke: new Stroke({
+          color: clusterStrokeColor,
+        }),
+        fill: new Fill({
+          color: clusterFillColor,
+        }),
+      }),
+      text: new Text({
+        text: size.toString(),
+        fill: new Fill({
+          color: clusterTextColor,
+        }),
+      }),
+    });
+  } else {
+    style = createStyle();
+  }
+  return style;
+}
 
 function Map({ APIurl }: MapProps) {
   const points: Point[] = [];
@@ -52,6 +110,10 @@ function Map({ APIurl }: MapProps) {
       data.forEach((pin: any) => {
         points.push(new Point(fromLonLat([pin.longitude, pin.latitude])));
       });
+      points.push(new Point(fromLonLat([10.887347709725966, 56.40611350358486]))); // Add a point to test clustering
+      points.push(new Point(fromLonLat([10.887347709725966, 5.40611350358486])));
+      points.push(new Point(fromLonLat([10.887347709725966, 5.40611650358486])));
+      points.push(new Point(fromLonLat([10.887347709725966, 5.40611450358486])));
     }
 
     const view = new View({
@@ -67,6 +129,12 @@ function Map({ APIurl }: MapProps) {
         return feature;
       }),
     });
+
+    const clusterSource = new Cluster({
+      distance: distanceBetweenPinsBeforeClustering,
+      source: vectorSource,
+    });
+
     const map = new OlMap({
       target: "map",
       controls: defaultControls().extend([mousePositionControl]),
@@ -75,7 +143,10 @@ function Map({ APIurl }: MapProps) {
           source: new OSM(),
         }),
         new VectorLayer({
-          source: vectorSource,
+          style: function (feature: any) {
+            return createClusterStyle(feature);
+          },
+          source: clusterSource,
         }),
       ],
       view: view,
@@ -97,7 +168,31 @@ function Map({ APIurl }: MapProps) {
       }
     });
 
-    // Add click event listener to open ShowPinModal
+    // Add click event listener to open ShowPinModal or zoom in on cluster
+    map.on("click", (event) => {
+      map.forEachFeatureAtPixel(event.pixel, (feature) => {
+        const features = feature.get('features');
+        if (features.length > 1) {
+          // Zoom in on cluster
+          const extent = createEmpty();
+          features.forEach((feature: Feature) => {
+            extend(extent, feature.getGeometry()?.getExtent() as Extent);
+          });
+          map.getView().fit(extent, { duration: 1000 });
+        } else {
+          // Open ShowPinModal
+          const pinIndex = feature.getId();
+          if (pinIndex !== undefined && data[pinIndex] !== undefined) {
+            const pinData = data[pinIndex];
+            setSelectedPin(pinData);
+            openShowPinModal();
+          }
+        }
+      });
+    });
+  }, [data]);
+
+    /* // Add click event listener to open ShowPinModal
     map.on("click", (event) => {
       map.forEachFeatureAtPixel(event.pixel, (feature) => {
         const pinIndex = feature.getId();
@@ -108,7 +203,7 @@ function Map({ APIurl }: MapProps) {
         }
       });
     });
-  }, [data]);
+  }, [data]); */
 
   const getBrowserLocation = (map: OlMap, view: View) => {
     if (!navigator.geolocation) return;
@@ -127,7 +222,10 @@ function Map({ APIurl }: MapProps) {
 
   return (
     <>
-      {data && <div id="map"></div>}
+      {data && <div id="map">
+        {/* Preload the image cause else React/Ionic will not load it and add it to the public folder */}
+        <IonImg src="/icons/webp/ping1.webp" alt="Pin Icon" style={{display:"none"}} aria-hidden="true" />
+      </div>}
       <IonModal isOpen={showPinModal} onDidDismiss={closeShowPinModal}>
         <div className="modal-content">
           <IonButton
