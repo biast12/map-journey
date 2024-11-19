f58d3a822517b14262df0b596c9704c9a1e40411
Noopel <30575718+Noopel@users.noreply.github.com>
2024-11-19 09:50:18 +0100

    Small changes

--- Frontend/src/components/admin/UserManagement.tsx
+++ Frontend/src/components/admin/UserManagement.tsx
@@ -6,6 +6,8 @@ import useRequestData from "../../hooks/useRequestData";
 import UserColumn from "./UserColumn";
 import Modal from "../Modal";
 
+import "./UserManagement.scss"
+
 type UserData = {
   avatar: string;
   banner: string;
@@ -26,18 +28,17 @@ const UserManagement = () => {
   const [showEditModal, setShowEditModal] = useState<boolean>(false);
   const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
 
-  function handleUserEdit(e: FormEvent , userData: UserData) {
+  function handleUserEdit(e: FormEvent, userData: UserData) {
     //Perform edit request based changes to their original data
-    
-    setShowEditModal(false)
-    setSelectedUser(null)
+    setShowEditModal(false);
+    setSelectedUser(null);
   }
   function handleUserDelete(userData: UserData) {
     //Perform delete request
     //Update user list according to response
 
-    setShowDeleteModal(false)
-    setSelectedUser(null)
+    setShowDeleteModal(false);
+    setSelectedUser(null);
   }
 
   useEffect(() => {
@@ -47,7 +48,39 @@ const UserManagement = () => {
   return (
     <>
       <Modal isOpen={showEditModal} onCloseModal={() => setShowEditModal(false)}>
-        {selectedUser && Object.entries(selectedUser).map((value)=><p>{value.join(": ")}</p>)}
+        {selectedUser && (
+          <section id="editUserModal">
+            <h3>Edit user</h3>
+            <figure>
+              <img src={selectedUser.banner} alt="User banner" />
+            </figure>
+            <figure>
+              <img src={selectedUser.avatar} alt="User avatar" />
+            </figure>
+            <form>
+              <input name="name" type="text" placeholder="Name" required defaultValue={selectedUser.name} />
+              <select name="role" id="">
+                <option selected={selectedUser.role === "user"} value="user">
+                  User
+                </option>
+                <option selected={selectedUser.role === "admin"} value="admin">
+                  Admin
+                </option>
+              </select>
+              <select name="status" id="">
+                <option selected={selectedUser.status === "public"} value="public">
+                  Public
+                </option>
+                <option selected={selectedUser.status === "private"} value="private">
+                  Private
+                </option>
+                <option selected={selectedUser.status === "reported"} value="reported">
+                  Reported
+                </option>
+              </select>
+            </form>
+          </section>
+        )}
       </Modal>
       <Modal isOpen={showDeleteModal} onCloseModal={() => setShowDeleteModal(false)}>
         {selectedUser && <IonButton onClick={() => handleUserDelete(selectedUser)}>Are you sure?</IonButton>}
